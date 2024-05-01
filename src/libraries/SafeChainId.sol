2882d8e5e751e24d042a537084c3fffce51c0f5c
Ryan Holanda <ryanholanda08@gmail.com>
2024-05-01 18:12:01 -0300

    chore: updated linter pre-commit error message

--- src/libraries/SafeChainId.sol
+++ src/libraries/SafeChainId.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.25;
 
+// solhint-disable chainlink-solidity/prefix-internal-functions-with-underscore
 library SafeChainId {
 	uint256 internal constant SEPOLIA_ID = 11155111;
 	uint256 internal constant ANVIL_ID = 31337;
